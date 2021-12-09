b20fc10da3c4bf838fd70a98f3f648d3bf056b5b
yasir <hellopleasantcodes@gmail.com>
2021-12-09 22:15:21 +0100

    created validation function

--- app.js
+++ app.js
@@ -24,17 +24,48 @@ UI.prototype.addBookToList = function(book) {
 
 }; // this adds the addBookToList function to the UI prototype
 
+UI.prototype.showAlert = function(message, className) {
+    // CREATION OF DIV ELEMENT
+  const div = document.createElement('div');
+  div.className = `alert ${className}`;
+  div.appendChild(document.createTextNode(message));
+
+    // ASSIGNING VARIABLES
+  const container = document.querySelector('.container');
+  const form = document.querySelector('#book_form');
+
+  container.insertBefore(div, form);
+  setTimeout(function() {
+    document.querySelector('.alert').remove();
+  }, 3000); // this inserts the div before the form element in the container, then takes it out after 3secs
+}; // this activates the alerts in the UI
+
+UI.prototype.clearFields = function() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+}; // this function clears the form input fields
 
   // DECLARING EVENT LISTENERS
 document.getElementById('book_form').addEventListener('submit', function(e){
+    // ASSIGNING VARIABLES
   const title = document.getElementById('title').value,
         author = document.getElementById('author').value,
         isbn = document.getElementById('isbn').value;
 
   const book = new Book(title, author, isbn); // this instantiates the book to an object
+
   const ui = new UI(); // this instantiates the UI
 
-  ui.addBookToList(book);
+  if(title === '' || author === '' || isbn === '') {
+    ui.showAlert('Do fill in all the apropriate fields', 'error');
+  } else {
+    ui.showAlert('Book added successfully!', 'success');
+
+    ui.addBookToList(book);
+    
+    ui.clearFields();
+  }
   
   e.preventDefault();
 });
