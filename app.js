dce720572b6b60b8aba0f5bc59ca9c90cec07d86
yasir <hellopleasantcodes@gmail.com>
2021-12-16 13:48:49 +0100

    ran put, post, and delete methods

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 const http = new easyHTTP();
 
-  // GET POST
+  // GET POST | GET METHOD
 http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
   if(err) {
     console.log(err);
   } else {
     console.log(posts);
+  }
+});
+
+  // CREATE POST | POST METHOD
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+}
+
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(post);
+  }
+});
+
+  // UPDATE POST | PUT METHOD
+http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(post);
+  }
+});
+
+  // DELETE POST | DELETE METHOD
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(response);
   }
 });
--- easyhttp.js
+++ easyhttp.js
@@ -19,3 +19,52 @@ easyHTTP.prototype.get = function(url, callback) {
 
   this.http.send();
 }
+
+  // POST REQUEST
+easyHTTP.prototype.post = function(url, data, callback) {
+  this.http.open('POST', url, true);
+
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+
+  this.http.onload = function() {
+    callback(null, self.http.responseText);   
+  }
+
+  this.http.send(JSON.stringify(data));
+}
+
+  // PUT REQUEST
+easyHTTP.prototype.put = function(url, data, callback) {
+  this.http.open('PUT', url, true);
+
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+
+  this.http.onload = function() {
+    callback(null, self.http.responseText);   
+  }
+
+  this.http.send(JSON.stringify(data));
+}
+
+  // DELETE REQUEST
+easyHTTP.prototype.delete = function(url, callback) {
+  this.http.open('DELETE', url, true);
+
+  let self = this;
+
+  this.http.onload = function() {
+    if(self.http.status === 200) {
+      callback(null, "Post deleted");
+    } else {
+      callback('Error:' + self.http.status)
+      console.log('Error: ' + self.status);
+    }
+  }
+
+  this.http.send();
+}
+  
