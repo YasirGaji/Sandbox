8730fab92c2aab5135787694878e878ee13fecf2
yasir <hellopleasantcodes@gmail.com>
2021-12-17 11:47:03 +0100

    did the post and put method to tweak the Api data

--- easyhttp2.js
+++ easyhttp2.js
@@ -9,11 +9,44 @@
  * 
  */
 
-class EasyHttp {
+class easyHttp {
+
     // GET REQUEST
   get(url) {
     return new Promise((resolve, reject) => {
       fetch(url)
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
+
+    // POST REQUEST
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
+
+    // PUT REQUEST
+  put(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'PUT',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
         .then(res => res.json())
         .then(data => resolve(data))
         .catch(err => reject(err));
