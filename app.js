3ada2f9cefd9733d80783ffc6082f68034f31665
yasir <hellopleasantcodes@gmail.com>
2021-12-20 13:53:54 +0100

    persisted info values to local storage

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
   // INSTANTIATING WEATHER OBJECT
-const weather = new Weather('Lagos', 'Nigeria');
-      ui = new UI();
+const ui = new UI();
+      storage = new Storage();
+      weatherLocation = storage.getLocationData();
+      weather = new Weather(weatherLocation.city, weatherLocation.state);
+      
 
+      
+
+      // EVENT LISTENERS
 document.addEventListener('DOMContentLoaded', getWeather);
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+
+  weather.changeLocation(city, state);
+
+  storage.setLocationData(city, state);
+
+  getWeather();
+
+  $('#locModal').modal('hide');
+})
 
 function getWeather() {
   weather.getWeather()
