0fd985a90f1dd601a3caf035f2f6f67ddba9ca20
yasir <hellopleasantcodes@gmail.com>
2021-12-21 13:18:38 +0100

    configured ui controllers and made dynamic rendering of the items into the interface

--- app.js
+++ app.js
@@ -30,21 +30,62 @@ const itemCtrl = (function(){
   }
 
   return {
+    getItems: function() {
+      return data.items;
+    },
+
     logData: function() {
       return data;
     }
   }
 })(); // The Item Controller
 
-const UICtrl = (function(){
 
+
+const UICtrl = (function(){
+  const UISelectors = {
+    itemList: '#item-list',
+  }
+
+    // PUBLIC METHODS
+  return {
+    populateItemList: function(items) {
+      let html = '';
+
+      items.forEach(function(item){
+        html += `
+          <li class="collection-item" id="item-${item.id}">
+            <div>
+              <strong>Meal: </strong>
+              <span class="name">${item.name}</span>
+              <strong>Calories: </strong>
+              <span class="calories">${item.calories}</span>
+              <a href="#" class="secondary-content">
+                <i class="edit-item fa fa-pencil"></i>
+              </a>
+            </div>
+          </li>
+        `;
+      });
+
+        // INSERT LIST ITEMS
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    }
+  }
 })(); // The UI Controller
+
+
 
 const App = (function(itemCtrl, UICtrl){
 
+    // THE PUBLIC METHODS
   return {
     init: function() {
       console.log('Application has started');
+
+      const Items = itemCtrl.getItems();
+
+      UICtrl.populateItemList(Items);
     }
   }
 
