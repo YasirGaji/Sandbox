454e8e726e4b28af8c63e521641ff846ec3c527b
yasir <hellopleasantcodes@gmail.com>
2022-01-05 06:53:28 +0100

    added a fake easy http library

---
+++ src/easyHttp.js
@@ -0,0 +1,61 @@
+/**
+ * EasyHTTP Library 3
+ * library for making HTTP requests
+ * 
+ * 
+ * @version 3.0
+ * @author Yasir Gaji
+ * @license PCI
+ * 
+ */
+
+ class EasyHttp {
+
+  // GET REQUEST
+async get(url) {
+  const response = await fetch(url);
+
+  const resData = await response.json();
+  return resData;
+}
+
+  // POST REQUEST
+async post(url, data) {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+  const resData = await response.json();
+  return resData;
+}
+
+  // PUT REQUEST
+async put(url, data) {
+  const response = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+  const resData = await response.json();
+  return resData;
+}
+
+  // DELETE REQUEST
+async delete(url) {
+  const response = await fetch(url, {
+    method: 'DELETE',
+    headers: {
+      'content-type': 'application/json'
+    },
+  });
+  const resData = await 'Resource Deleted';
+  return resData;
+}
+}
+
+export const http = new EasyHttp();
