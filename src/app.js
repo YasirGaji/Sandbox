662fccac6a45e2d64ee487bc3ac4d3e6382d5d1a
yasir <hellopleasantcodes@gmail.com>
2022-01-07 11:00:56 +0100

    configured add post function and the show alert function

--- src/app.js
+++ src/app.js
@@ -17,9 +17,30 @@ import { ui } from './ui';
   // GET POSTS ON DOM LOAD
 document.addEventListener('DOMContentLoaded', getPosts);
 
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
 function getPosts() {
   http.get('http://localhost:3000/posts')
   .then(data => ui.showPosts(data))
+  .catch(err => console.log(err));
+}
+
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title,
+    body
+  }
+
+  http.post('http://localhost:3000/posts', data)
+  .then(data => {
+    ui.showAlert('Post added', 'alert alert-success');
+    ui.clearFields();
+
+    getPosts();
+  })
   .catch(err => console.log(err));
 }
 
--- src/ui.js
+++ src/ui.js
@@ -33,6 +33,36 @@ class UI {
     
     this.post.innerHTML = output;
   }
+
+  showAlert(message, className) {
+    this.clearAlert();
+
+    const div = document.createElement('div');
+    div.className = className;
+
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.postsContainer');
+
+    const posts = document.querySelector('#posts');
+    container.insertBefore(div,posts);
+
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+
+    if(currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+  clearFields() {
+    this.titleInput.value = '';
+    this.bodyInput.value = '';
+  }
 }
 
 export const ui = new UI();
